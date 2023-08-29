62ab7c8801fa0d32c034daa0dc0609976bb48ebb
privhakim <abdihakim.hassan@student.moringaschool.com>
2023-08-29 17:52:22 +0300

    done

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,32 @@
 from config import CONN, CURSOR
 
 class Song:
-    pass
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(self):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+        "    )
+        """   
+
+        CURSOR.excute(sql)   
+
+    def save(self):
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
+
+        CURSOR.excute(sql, (self.name, self.album)) 
+        
+        self.id = CURSOR.execute("SELECT last_insert_rowid() FROM songs").fetchone()[0]   
+
+  
+    
